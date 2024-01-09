62915ad05f7bed3a0bdaa78edb696bb6c6d6f630
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2024-01-09 17:53:47 +0100

    Create auth.js
    Création du middleware d'authentification

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const dotenv = require("dotenv");
+dotenv.config();
+const JWT_TOKEN = process.env.JWT_TOKEN;
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, JWT_TOKEN);
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
