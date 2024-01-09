eb68fc7148ac542357ef12b638b7936d2d336696
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2024-01-09 17:51:47 +0100

    update app.js
    Ajout des variable d'environnement avec le module dotenv
    Ajout de la route user

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const path = require('path');
 // variables d'environement module dotenv
 const dotenv = require("dotenv");
 dotenv.config();
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_LOGIN = process.env.DB_LOGIN;
+const DB_MONGODB = process.env.DB_MONGODB;
+
+// Routes
+const userRoutes = require('./routes/user');
+
+const app = express();
 
 // connexion a MongoDB via le module mongoose
 mongoose.connect(`mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}${DB_MONGODB}`,
@@ -12,3 +20,9 @@ mongoose.connect(`mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}${DB_MONGODB}`,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
