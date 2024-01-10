8105bf5b1653f2c61ee8777276e853dea7b8746f
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2024-01-10 17:45:23 +0100

    création route page /signup
    création de la page signup

---
+++ client/app/signup/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignupForm from '../components/SignupForm/SignupForm'
+
+function signupPage() {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  )
+}
+
+signupPage.propTypes = {}
+
+export default signupPage
