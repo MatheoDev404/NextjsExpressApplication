14a930b9be67cb2742f09727f74b72b6267650d4
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2024-01-10 17:46:31 +0100

    ajout parametreage header
    parametrage des header pour le CORS

--- server/app.js
+++ server/app.js
@@ -23,6 +23,14 @@ mongoose.connect(`mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}${DB_MONGODB}`,
 
 app.use(express.json());
 
-app.use('/api/auth', userRoutes);
+// Paramètrage des Header || Cross-origin resource sharing (CORS)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api', userRoutes);
 
 module.exports = app;
